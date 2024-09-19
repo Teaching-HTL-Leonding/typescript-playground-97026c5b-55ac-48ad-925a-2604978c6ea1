5f5eebfe0ad316d7f71ee120da1242e7f71bc8d6
Rainer Stropek <rainer@timecockpit.com>
2024-09-19 09:21:01 +0200

    Saved in TypeScript Playground at 2024-09-19T07:21:01.529Z

--- house/index.ts
+++ house/index.ts
@@ -3,26 +3,20 @@ function setup() {
     background("#E0FFFF");
 
     fill("#FF4500"); //Wand
-    rect(90, 250, 200, 220);
+    rect(90, 280, 200, 190);
 
     fill("#FFD700"); //Tür
     rect(165, 400, 50, 70);
 
     fill("#8B4513"); //Dach
-    triangle(70, 250, 310, 250, 190, 160);
+    triangle(70, 280, 310, 280, 190, 160);
 
     fill("brown"); //Stamm
-    rect(380, 390, 35, 80);
+    rect(380, 280, 35, 190);
 
     fill("green"); //Blätter
-    circle(395, 350, 70);
-    circle(420, 380, 70);
-    circle(370, 380, 70);
-    
-
-
-
-    
-
+    circle(395, 270, 80);
+    circle(420, 300, 80);
+    circle(370, 300, 80);
 
 }
