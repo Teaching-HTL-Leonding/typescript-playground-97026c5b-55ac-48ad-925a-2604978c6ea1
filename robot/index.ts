9ce54d03a1d72a076aa7e8eceb1c587a0bea8b0e
Rainer Stropek <rainer@timecockpit.com>
2024-09-19 10:40:23 +0200

    Saved in TypeScript Playground at 2024-09-19T08:40:23.264Z

--- robot/index.ts
+++ robot/index.ts
@@ -3,6 +3,7 @@ function setup() {
     background("lightgrey");
 
     //Roboterkopf
+    strokeWeight(3);
     fill("grey");
     rect(100, 150, 300, 300);
 
@@ -10,5 +11,13 @@ function setup() {
     fill("black");
     circle(150, 220, 60);
     circle(350, 220, 60);
+
+    //Antenne
+    line(250, 150, 250, 50);
+    fill("white");
+    circle(250, 50, 40)
+
+    //Mund
+    rect(170, 330, 170, 40);
 
 }
