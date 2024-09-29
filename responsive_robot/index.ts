9f6a90f48a3e3679cb0f696a3246fe0e54d77b59
Rainer Stropek <rainer@timecockpit.com>
2024-09-29 12:47:18 +0200

    Saved in TypeScript Playground at 2024-09-29T10:47:17.838Z

---
+++ responsive_robot/index.ts
@@ -0,0 +1,34 @@
+function setup() {
+  createCanvas(400, 400);
+  background("lightgray");
+
+  // Define relative sizes and positions based on canvas width and height
+  const headWidth = width * 0.50; // Head size as 50% of the canvas width
+  const headHeight = height * 0.50; // Head size as 50% of the canvas width
+  const headX = width / 2 - headWidth / 2; // Center the head horizontally
+  const headY = height / 2 - headHeight / 2; // Center the head vertically
+  const eyed = headWidth * 0.2;
+  const viertelkopfx = headWidth * 0.75;
+  const dreiviertelkopfx = headWidth * 1.25;
+  const viertelkopfy = headHeight * 0.75;
+  const mundY = headHeight * 1.2;
+  const mundlaenge = headWidth * 0.5;
+  const mundhoehe = headHeight * 0.1;
+
+  // Draw the robot's head
+  fill("gray");
+  rect(headX, headY, headWidth, headHeight);
+
+  fill("black");
+  circle(viertelkopfx, viertelkopfy, eyed);//auge links
+  circle(dreiviertelkopfx, viertelkopfy, eyed);//auge rechts
+  
+  fill("white");
+  rect(viertelkopfx, mundY, mundlaenge, mundhoehe); // Mund
+
+
+
+
+
+  // Add your code here
+}
