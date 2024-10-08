530a9ea464d62ae00fcd2a7091df31ed3e646193
Rainer Stropek <rainer@timecockpit.com>
2024-10-08 17:22:04 +0200

    Saved in TypeScript Playground at 2024-10-08T15:22:03.488Z

---
+++ bouncing_ball/index.ts
@@ -0,0 +1,38 @@
+function setup() {
+  createCanvas(300, 200);
+}
+
+const circleDiameter = 50;
+
+let circleCenterX = 0;
+let circleCenterY = 0;
+let direction = 2;
+
+// Remember: The _draw_ method is called FOR EVERY FRAME
+function draw() {
+  background("gold");
+
+  stroke("white");
+  strokeWeight(3);
+  fill("lime");
+  circle(circleCenterX, height / 2, circleDiameter);
+
+  circleCenterX += direction;
+
+  //                         +----------------------------- OR operator
+  //                         |
+  //                         v
+  if (circleCenterX >= width || circleCenterX <= 0) {
+    // Reverse sign of direction (positive -> right, negative -> left)
+    direction *= -1; 
+  }
+
+  fill("cyan");
+  circle(width / 2, circleCenterY, circleDiameter);
+
+  circleCenterY += direction;
+ 
+  if(circleCenterY >= height || circleCenterY <= 0) {
+    direction*= -1;
+  }
+}
