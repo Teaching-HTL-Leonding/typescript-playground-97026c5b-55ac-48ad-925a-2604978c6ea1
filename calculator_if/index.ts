c4d79fe346df6df650885cfdbd166eb4db2a4c4f
Rainer Stropek <rainer@timecockpit.com>
2024-11-05 13:47:28 +0100

    Saved in TypeScript Playground at 2024-11-05T12:47:28.388Z

--- calculator_if/index.ts
+++ calculator_if/index.ts
@@ -5,7 +5,7 @@ let lineHeight: number = 0;   // Height of a line of the responsive layout
 let cellWidth: number = 0;    // Width of a cell of the responsive layout
 
 function setup() {
-    createCanvas(300, 600);
+    createCanvas(300, 400);
     lineHeight = height / 5;
     cellWidth = width / 3;
 }
@@ -14,7 +14,7 @@ function draw() {
     background("lightgray");
 
 
-    fill("white");  
+    fill("white");
     rect(width / 30, height / 60, width * 0.925, height / 6);
 
     fill("black");
@@ -27,69 +27,64 @@ function draw() {
 
     textAlign(CENTER, CENTER);
     textSize(50);
-    text("1", width / 6, height * 1.5 / 5);
-    text("2", width / 2, height * 1.5 / 5);
-    text("3", width * 5 / 6, height * 1.5 / 5);
+    text("7", width / 6, height * 1.5 / 5);
+    text("8", width / 2, height * 1.5 / 5);
+    text("9", width * 5 / 6, height * 1.5 / 5);
     text("4", width / 6, height * 2.5 / 5);
     text("5", width / 2, height * 2.5 / 5);
     text("6", width * 5 / 6, height * 2.5 / 5);
-    text("7", width / 6, height * 3.5 / 5);
-    text("8", width / 2, height * 3.5 / 5);
-    text("9", width * 5 / 6, height * 3.5 / 5);
+    text("1", width / 6, height * 3.5 / 5);
+    text("2", width / 2, height * 3.5 / 5);
+    text("3", width * 5 / 6, height * 3.5 / 5);
     text("C", width * 5 / 6, height * 4.5 / 5);
     text("0", width / 3, height * 4.5 / 5);
 
     textAlign(RIGHT, CENTER);
     fill("black");
     text(displaynumber, width * 0.85, height / 10);
-   
+
 
     // <<< Add code here
 }
 
-    let displaynumber: string = "";
+let displaynumber: number = "";
 function mouseClicked() {
+    fill("white");
+    rect(width / 30, height / 60, width * 0.925, height / 6);
 
 
-
-if(mouseX > 0 && mouseX < cellWidth && mouseY > lineHeight && mouseY < lineHeight * 2){
-displaynumber += "1";
-}
-if(mouseX > cellWidth && mouseX < cellWidth * 2 && mouseY > lineHeight && mouseY < lineHeight * 2){
-displaynumber += "2";
-}
-if(mouseX > cellWidth * 2 && mouseX < cellWidth * 3 && mouseY > lineHeight && mouseY < lineHeight * 2){
-displaynumber += "3";
-}
-if(mouseX > 0 && mouseX < cellWidth && mouseY > lineHeight * 2 && mouseY < lineHeight * 3){
-displaynumber += "4";
-}
-if(mouseX > cellWidth && mouseX < cellWidth * 2 && mouseY > lineHeight * 2 && mouseY < lineHeight * 3){
-displaynumber += "5";
-}
-if(mouseX > cellWidth * 2 && mouseX < cellWidth * 3 && mouseY > lineHeight * 2 && mouseY < lineHeight * 3){
-displaynumber += "6";
-}
-if(mouseX > 0 && mouseX < cellWidth && mouseY > lineHeight * 3 && mouseY < lineHeight * 4){
-displaynumber += "7";
-}
-if(mouseX > cellWidth  && mouseX < cellWidth * 2 && mouseY > lineHeight * 3 && mouseY < lineHeight * 4){
-displaynumber += "8";
-}
-if(mouseX > cellWidth * 2 && mouseX < cellWidth * 3 && mouseY > lineHeight * 3 && mouseY < lineHeight * 4){
-displaynumber += "9";
-}
-if(mouseX > 0 && mouseX < cellWidth * 2 && mouseY > lineHeight * 4 && mouseY < lineHeight * 5){
-displaynumber += "0";
-}
-if(mouseX > cellWidth * 2 && mouseX < cellWidth * 3 && mouseY > lineHeight * 4 && mouseY < lineHeight * 5){
-displaynumber = "0";
-fill("white");  
- rect(width / 30, height / 60, width * 0.925, height / 6);
-
-} 
-const oldNum = displaynumber;
-        if (displaynumber >= 1000000000) {
-            num = oldNum;
+    if (mouseX > 0 && mouseX < cellWidth && mouseY > lineHeight && mouseY < lineHeight * 2) {
+        displaynumber = displaynumber * 10 + 7;
+    }
+    if (mouseX > cellWidth && mouseX < cellWidth * 2 && mouseY > lineHeight && mouseY < lineHeight * 2) {
+        displaynumber = displaynumber * 10 + 8;
+    }
+    if (mouseX > cellWidth * 2 && mouseX < cellWidth * 3 && mouseY > lineHeight && mouseY < lineHeight * 2) {
+        displaynumber = displaynumber * 10 + 9;
+    }
+    if (mouseX > 0 && mouseX < cellWidth && mouseY > lineHeight * 2 && mouseY < lineHeight * 3) {
+        displaynumber = displaynumber * 10 + 4;
+    }
+    if (mouseX > cellWidth && mouseX < cellWidth * 2 && mouseY > lineHeight * 2 && mouseY < lineHeight * 3) {
+        displaynumber = displaynumber * 10 + 5;
+    }
+    if (mouseX > cellWidth * 2 && mouseX < cellWidth * 3 && mouseY > lineHeight * 2 && mouseY < lineHeight * 3) {
+        displaynumber = displaynumber * 10 + 6;
+    }
+    if (mouseX > 0 && mouseX < cellWidth && mouseY > lineHeight * 3 && mouseY < lineHeight * 4) {
+        displaynumber = displaynumber * 10 + 1;
+    }
+    if (mouseX > cellWidth && mouseX < cellWidth * 2 && mouseY > lineHeight * 3 && mouseY < lineHeight * 4) {
+        displaynumber = displaynumber * 10 + 2;
+    }
+    if (mouseX > cellWidth * 2 && mouseX < cellWidth * 3 && mouseY > lineHeight * 3 && mouseY < lineHeight * 4) {
+        displaynumber = displaynumber * 10 + 3;
+    }
+    if (mouseX > 0 && mouseX < cellWidth * 2 && mouseY > lineHeight * 4 && mouseY < lineHeight * 5) {
+        displaynumber = displaynumber * 10 + 0;
+    }
+    if (mouseX > cellWidth * 2 && mouseX < cellWidth * 3 && mouseY > lineHeight * 4 && mouseY < lineHeight * 5)
+        displaynumber = displaynumber - displaynumber;
+        
     }
     // <<< Add code here
