177bdf1f84b216b9974eefed3ce040ea7c00d088
Rainer Stropek <rainer@timecockpit.com>
2024-11-06 19:25:42 +0100

    Saved in TypeScript Playground at 2024-11-06T18:25:42.128Z

--- binary_guesser/index.ts
+++ binary_guesser/index.ts
@@ -19,7 +19,6 @@ function setup() {
   text(`${num} in binary ?`, width / 2, height / 5);
 
   //textAlign(CENTER, CENTER);
-  textSize(20);
   text(stelle1, width * 1.5 / 8, height * 6 / 12);
   text(stelle2, width * 3 / 8, height / 2);
   text(stelle3, width * 4.5 / 8, height / 2);
@@ -35,11 +34,41 @@ function mouseClicked() {
     if(stelle1 === 1){
       stelle1 = 0;
     } else {stelle1 = 1;}
+    fill("black");
+    rect(width / 8, height / 3, width / 8, height / 4);
+    fill("yellow");
+    text(stelle1, width * 1.5 / 8, height * 6 / 12);
   }
   if(mouseX >= width * 2.5 / 8 && mouseX <= width * 3.5 / 8 && mouseY >= height / 3 && mouseY <= height * 3 / 6){
     if(stelle2 === 1){
       stelle2 = 0;
     } else{stelle2 = 1;}
+    fill("black");
+    rect(width * 2.5 / 8, height / 3, width / 8, height / 4);
+    fill("yellow");
+    text(stelle2, width * 3 / 8, height / 2);
+  } 
+  if(mouseX >= width / 2 && mouseX <= width * 5 / 8 && mouseY >= height / 3 && mouseY <= height * 3 / 6){
+    if(stelle3 === 1){
+      stelle3 = 0;
+    }else{stelle3 = 1;}
+    fill("black");
+    rect(width / 2, height / 3, width / 8, height / 4);
+    fill("yellow");
+    text(stelle3, width * 4.5 / 8, height / 2);
   }
-  
+  if(mouseX >= width * 5.5 / 8 && mouseX <= width * 7 / 8 && mouseY >= height / 3 && mouseY <= height * 3 / 6){
+    if(stelle4 === 1){
+      stelle4 = 0;
+    } else{stelle4 = 1;}
+    fill("black");
+    rect(width * 5.5 / 8, height / 3, width / 8, height / 4);
+    fill("yellow");
+    text(stelle4, width * 6 / 8, height / 2);
+  }
+  let result = "";
+  if(stelle1 === Math.pow(2,3) && stelle2 === Math.pow(2,2) && stelle3 === Math.pow(2,1) && stelle4 === Math.pow(2,0)){
+    result = "Correct!";
+  }
+    text(result, width / 2, height * 3 / 4);
 }
