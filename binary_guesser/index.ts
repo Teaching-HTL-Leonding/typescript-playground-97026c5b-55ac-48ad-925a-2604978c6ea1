33c5a5c509eac127eedd208374a6e417f6b47ca8
Rainer Stropek <rainer@timecockpit.com>
2024-11-06 18:47:03 +0100

    Saved in TypeScript Playground at 2024-11-06T17:47:02.989Z

--- binary_guesser/index.ts
+++ binary_guesser/index.ts
@@ -1,97 +1,37 @@
-
 let num = 0;
 
 function setup() {
   num = Math.floor(random(0, 16));
 
-
   createCanvas(400, 300);
   background("black");
-  stroke("yellow");
+
   noFill();
-  rect(width * 0.1, height / 3, width / 7, height / 3);
-  rect(width * 0.325, height / 3, width / 7, height / 3);
-  rect(width * 0.55, height / 3, width / 7, height / 3);
-  rect(width * 0.75, height / 3, width / 7, height / 3);
+  stroke("yellow");
+  rect(width / 8, height / 3, width / 8, height / 4);
+  rect(width * 2.5 / 8, height / 3, width / 8, height / 4);
+  rect(width / 2, height / 3, width / 8, height / 4);
+  rect(width * 5.5 / 8, height / 3, width / 8, height / 4);
 
   fill("yellow");
+  textSize(30);
+  textAlign(CENTER);
+  text(`${num} in binary ?`, width / 2, height / 5);
+
   textAlign(CENTER, CENTER);
   textSize(20);
-  text(`${num} in binary ?`, width / 2, height / 8)
-
-
-  text("0", width * 0.1, height / 3, width / 7, height / 3);
-  text("0", width * 0.325, height / 3, width / 7, height / 3);
-  text("0", width * 0.55, height / 3, width / 7, height / 3);
-  text("0", width * 0.75, height / 3, width / 7, height / 3);
-
+  text(stelle1, width / 5, height * 6 / 12);
+  // <<< Add your code here
 }
-let stelle1 = 0;
-let stelle2 = 0;
-let stelle3 = 0;
-let stelle4 = 0;
-
+let stelle1 = "0";
+let stelle2 
+let stelle3
+let stelle4
 function mouseClicked() {
-
-
-  textAlign(CENTER, CENTER);
-  if (mouseX > width * 0.1 && mouseX < (width * 0.1 + width / 7) && mouseY > height / 3 && mouseY < (height / (2 / 3))) {
-    fill("black");
-    rect(width * 0.1, height / 3, width / 7, height / 3);
-    fill("yellow");
-    if (stelle1 === 0) {
-      stelle1 = 1;
-    } else {
-      stelle1 = 0;
-    }
-    text(`${stelle1}`, width * 0.1, height / 3, width / 7, height / 3);
-  } if (mouseX > width * 0.325 && mouseX < (width * 0.325 + width / 7) && mouseY > height / 3 && mouseY < (height / (2 / 3))) {
-    fill("black");
-    rect(width * 0.325, height / 3, width / 7, height / 3);
-    fill("yellow");
-    if (stelle2 === 0) {
-      stelle2 = 1;
-    } else {
-      stelle2 = 0;
-    }
-    text(`${stelle2}`, width * 0.325, height / 3, width / 7, height / 3);
-  } if (mouseX > width * 0.55 && mouseX < (width * 0.55 + width / 7) && mouseY > height / 3 && mouseY < (height / (2 / 3))) {
-    fill("black");
-    rect(width * 0.55, height / 3, width / 7, height / 3);
-    fill("yellow");
-    if (stelle3 === 0) {
-      stelle3 = 1;
-    } else {
-      stelle3 = 0;
-    }
-    text(`${stelle3}`, width * 0.55, height / 3, width / 7, height / 3);
-  } if (mouseX > width * 0.75 && mouseX < (width * 0.75 + width / 7) && mouseY > height / 3 && mouseY < (height / (2 / 3))) {
-    fill("black");
-    rect(width * 0.75, height / 3, width / 7, height / 3);
-    fill("yellow");
-    if (stelle4 === 0) {
-      stelle4 = 1;
-    } else {
-      stelle4 = 0;
-    }
-    text(`${stelle4}`, width * 0.75, height / 3, width / 7, height / 3);
+  if(mouseX >= width / 8 && mouseX <= width / 4 && mouseY >= height / 3 && mouseY <= height * 2 / 6){
+    if(stelle1 === ""){
+      stelle1 = "0";
+    } else {stelle1 = "1";}
   }
-
-  if (num === stelle1 * Math.pow(2, 3) + stelle2 * Math.pow(2, 2) + stelle3 * Math.pow(2, 1) + stelle4 * Math.pow(2, 0)) {
-    fill("black");
-    stroke("lightgreen");
-    rect(width * 0.1, height / 3, width / 7, height / 3);
-    rect(width * 0.325, height / 3, width / 7, height / 3);
-    rect(width * 0.55, height / 3, width / 7, height / 3);
-    rect(width * 0.75, height / 3, width / 7, height / 3);
-
-    fill("lightgreen");
-    text(`${stelle1}`, width * 0.1, height / 3, width / 7, height / 3);
-    text(`${stelle2}`, width * 0.325, height / 3, width / 7, height / 3);
-    text(`${stelle3}`, width * 0.55, height / 3, width / 7, height / 3);
-    text(`${stelle4}`, width * 0.75, height / 3, width / 7, height / 3);
-    text("Correct!", width / 2, height * 0.8);
-
-  }
-
+  
 }
