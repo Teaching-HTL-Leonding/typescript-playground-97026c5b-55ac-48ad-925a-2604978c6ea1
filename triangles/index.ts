535dab74cf3a76a0e383750bfa3e4300f61de6b4
Rainer Stropek <rainer@timecockpit.com>
2024-11-28 09:23:01 +0100

    Saved in TypeScript Playground at 2024-11-28T08:23:00.674Z

---
+++ triangles/index.ts
@@ -0,0 +1,28 @@
+function setup() {
+    const SIZE = 400;
+    const SIDE_LENGTH = 50;
+
+    // We have an isosceles triangle ("gleichschenkeliges Dreieck").
+    // This is the formula to calculate the height of such a triangle
+    const HEIGHT = SIDE_LENGTH * Math.sqrt(3) / 2;
+
+    createCanvas(SIZE, HEIGHT * 9);
+    background("black");
+
+    strokeWeight(1);
+    stroke("yellow");
+    noFill();
+
+    for(let x = 0; x <= SIZE; x += SIDE_LENGTH){
+        push();
+        for(let y = 0; y <= SIZE; y += SIDE_LENGTH){
+        triangle(0, SIDE_LENGTH, SIDE_LENGTH, SIDE_LENGTH, SIDE_LENGTH / 2, 0);
+        translate(SIDE_LENGTH, 0);
+        }
+
+        pop();
+        translate(0, SIDE_LENGTH);
+    }
+
+    // <<< Add code to draw the triangles here
+}
