158a197bd93bf6e7b73673562748210bc625e136
Rainer Stropek <rainer@timecockpit.com>
2024-12-04 15:42:25 +0100

    Saved in TypeScript Playground at 2024-12-04T14:42:22.987Z

--- christmas_tree/index.ts
+++ christmas_tree/index.ts
@@ -10,23 +10,26 @@ const TRUNK_HEIGHT = 50;
 function setup() {
   createCanvas(500, 500);
   background("black");
-  noStroke();
-  fill("brown");
-  rect(width / 2 - 25, height * 3 / 4 - 10, 50, 50);
-
- translate(width / 2, TOP_MARGIN);
- fill("green");
- let trianglewidth = INITIAL_TRIANGLE_BASE_WIDTH;
- let color1 = 0;
- for(let i = 0; i < NUMBER_OF_LAYERS; i ++){
   fill("green");
-  triangle(- trianglewidth / 2,trianglewidth / 2, 0, 0, trianglewidth / 2, trianglewidth / 2);
-  colorMode(HSB);
-  fill(color1, 50, 50);
-  circle(- trianglewidth / 2, trianglewidth / 2 + 15, SPHERE_DIAMETER);
-  circle(trianglewidth / 2, trianglewidth / 2 + 15, SPHERE_DIAMETER);
-  translate(0, trianglewidth / 4);
-  trianglewidth *= 1.5;
-  color1 += 50;
- }
+  noStroke();
+
+  // <<< Draw the tree
+
+  let trianglewidth = INITIAL_TRIANGLE_BASE_WIDTH;
+  let color1 = 0;
+  translate(width / 2 , TOP_MARGIN);
+  for(let i = 0; i < NUMBER_OF_LAYERS; i ++){
+    fill("green");
+    triangle(- trianglewidth / 2, trianglewidth / 2, 0, 0, trianglewidth / 2, trianglewidth / 2);
+    colorMode(HSB);
+    fill(color1, 80, 80);
+    circle(-trianglewidth / 2, trianglewidth / 2 + 15, SPHERE_DIAMETER);
+    circle(trianglewidth / 2, trianglewidth / 2 + 15, SPHERE_DIAMETER);
+    translate(0, trianglewidth / 4);
+    trianglewidth *= 1.5;
+    color1 += 60;
+    colorMode(RGB);
+    fill("brown");
+    rect(- TRUNK_WIDTH / 2, trianglewidth / 2, TRUNK_WIDTH, TRUNK_HEIGHT);
+  }
 }
