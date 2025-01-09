84ca37828394cd497236e42a2e4492bab7cf38e2
Rainer Stropek <rainer@timecockpit.com>
2025-01-09 08:57:31 +0100

    Saved in TypeScript Playground at 2025-01-09T07:57:30.978Z

--- moving_lines_exam/index.ts
+++ moving_lines_exam/index.ts
@@ -12,75 +12,86 @@ let lineEndX: number[] = [];       // End coordinates of line
 let lineEndY: number[] = [];
 let lineColor: number[] = [];
 
+let startlines: number = 4;
+
 let lineStartDx: number[] = [];    // Movement of start point per frame in X and Y direction
 let lineStartDy: number[] = [];
 let lineEndDx: number[] = [];      // Movement of end point per frame in X and Y direction
 let lineEndDy: number[] = [];
 
-lineStartDx.push(random(0, 5));
-lineStartDy.push(random(0, 5));
-lineEndDx.push(random(0, 5));
-lineEndDy.push(random(0, 5));
+
 
 let minColor = 0;               // Lower bound of random hue value
-let maxColor = 360;      
-       // Upper bound of random hue value
-
+let maxColor = 360;
+// Upper bound of random hue value
+const plus = "+";
+const minus = "-";
 
 function setup() {
-    
     createCanvas(500, 500);
     colorMode(HSB);
+    for (let i = 0; i < startlines; i++) {
 
-    // Set random start and end position
-    lineStartX = random(50, 450);
-    lineStartY = random(50, 450);
-    lineEndX = random(50, 450);
-    lineEndY = random(50, 450);
 
-    // Set random movement
-    lineStartDx = random(0, 5);
-    lineStartDy = random(0, 5);
-    lineEndDx = random(0, 5);
-    lineEndDy = random(0, 5);
+        // Set random start and end position
+        lineStartX.push(random(50, 450));
+        lineStartY.push(random(50, 450));
+        lineEndX.push(random(50, 450));
+        lineEndY.push(random(50, 450));
 
-    // Set random color
-    lineColor = random(minColor, maxColor);
-    
+        // Set random movement
+        lineStartDx.push(random(0, 5));
+        lineStartDy.push(random(0, 5));
+        lineEndDx.push(random(0, 5));
+        lineEndDy.push(random(0, 5));
+
+        // Set random color
+        lineColor.push(random(minColor, maxColor));
+    }
+
 }
 
 function draw() {
 
-    for(let i = 0; i <= 10; i ++){
     background("black");
+    for (let i = 0; i < startlines; i++) {
 
-    push();
-    // Draw current line
-    stroke(lineColor[i], 100, 100);
-    strokeWeight(2);
-    line(lineStartX[i], lineStartY[i], lineEndX[i], lineEndY[i]);
+        push();
+        // Draw current line
+        stroke(lineColor[i], 100, 100);
+        strokeWeight(2);
+        line(lineStartX[i], lineStartY[i], lineEndX[i], lineEndY[i]);
 
-    // Move start and end point
-    lineStartX[i] += lineStartDx[i];
-    lineStartY[i] += lineStartDy[i];
-    lineEndX[i] += lineEndDx[i];
-    lineEndY[i] += lineEndDy[i];
+        // Move start and end point
+        lineStartX[i] += lineStartDx[i];
+        lineStartY[i] += lineStartDy[i];
+        lineEndX[i] += lineEndDx[i];
+        lineEndY[i] += lineEndDy[i];
 
-    // Reverse direction when edge has been reached
-    if (lineStartX[i] < 0 || lineStartX[i] > width) {
-        lineStartDx[i] = -lineStartDx{i};
-    }
-    if (lineStartY[i] < 0 || lineStartY[i] > height) {
-        lineStartDy[i] = -lineStartDy[i];
-    }
+        // Reverse direction when edge has been reached
+        if (lineStartX[i] < 0 || lineStartX[i] > width) {
+            lineStartDx[i] = -lineStartDx[i];
+        }
+        if (lineStartY[i] < 0 || lineStartY[i] > height) {
+            lineStartDy[i] = -lineStartDy[i];
+        }
 
-    if (lineEndX[i] < 0 || lineEndX[i] > width) {
-        lineEndDx[i] = -lineEndDx[i];
-    }
-    if (lineEndY[i] < 0 || lineEndY[i] > height) {
-        lineEndDy[i] = -lineEndDy[i];
-    }
+        if (lineEndX[i] < 0 || lineEndX[i] > width) {
+            lineEndDx[i] = -lineEndDx[i];
+        }
+        if (lineEndY[i] < 0 || lineEndY[i] > height) {
+            lineEndDy[i] = -lineEndDy[i];
+        }
 
-    pop();
+        pop();
     }
+    // draw the buttons
+    fill("white");
+    rect(5, 5, 50, 50);
+    rect(60, 5, 50, 50);
+    fill("black");
+    textAlign(CENTER, CENTER);
+    textSize(35);
+    text(plus, 5 + 50 / 2, 5 + 50 / 2);
+    text(minus, 65 + 50 / 2, 5 + 50 / 2);
 }
