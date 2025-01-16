fda6ff3e737b807cb4f0925610668703a89e31c9
Rainer Stropek <rainer@timecockpit.com>
2025-01-16 10:32:07 +0100

    Saved in TypeScript Playground at 2025-01-16T09:32:06.871Z

--- parallax_step_1/index.ts
+++ parallax_step_1/index.ts
@@ -1,56 +1,40 @@
-// Index of the theme to use. 0 = cave, 1 = mountains, 2 = winter, 3 = dessert
-// IMPORTANT: Take a look at the file _images.ts_ to see the available themes and their indexes.
-// Make sure you understand the structure of the arrays in _images.ts_.
-const THEME_IX = 2;
+let backgroundimages: p5.Image[] = [];
 
-// Array with p5js images holding the image objects for the current theme
-const images: p5.Image[] = [];
-
-// The background images do not have the same size as the drawing canvas. Therefore,
-// we have to scale them to fit the canvas. The scale factor is calculated in the _setup()_ function.
-// The scale factor is the percentage of the canvas width that the background image will use.
-// Example: 0.5 means that the background image has to be scaled to 50% to fill the width of the canvas.
-// 1 would mean that the image fits perfectly into the canvas width (i.e. image size matches canvas size).
-let backgroundScale = 1;
-
-// Width and height of the scaled images
-let scaledImageWidth = 0;
-let scaledImageHeight = 0;
+let backgroundscale: number = 1;
+let scaledImagewidth: number = 0;
+let scaledImageheight: number = 0;
+let scrollposition: number = 0;
 
 function preload() {
-    // Load the images for the current theme. For that, we iterate over the image names of the current theme.
-    const theme = themes[THEME_IX];
-    for (let i = 0; i < theme.length; i++) {
-        const imageName = theme[i];
-        const image = loadImage(`${BASE_URL}/${themeNames[THEME_IX]}/${imageName}`);
-
-        // Add the p5js image to the array of p5js images
-        images.push(image);
+    for (let i = 0; i < winter.length; i++) {
+        backgroundimages.push(loadImage(`${BASE_URL}/winter/${winter[i]}`));
     }
 }
 
 function setup() {
     createCanvas(500, 500);
-
-    // Calculate the scale factor for the background images
-    backgroundScale = width / images[0].width;
-
-    // Calculate the scaled image width and height
-    scaledImageWidth = images[0].width * backgroundScale;
-    scaledImageHeight = images[0].height * backgroundScale;
+    
+    backgroundscale = width / backgroundimages[0].width;
+    scaledImagewidth = backgroundimages[0].width * backgroundscale;
+    scaledImageheight = backgroundimages[0].height * backgroundscale;
 }
 
 function draw() {
     background("black");
 
-    push();
-    // Center the images on the canvas
-    translate(0, (height - scaledImageHeight) / 2);
-
-    // Draw the images
-    for (let i = 0; i < images.length; i++) {
-        const img = images[i];
-        image(img, 0, 0, scaledImageWidth, scaledImageHeight);
+    if(keyIsDown(39)){
+        scrollposition -= 5;
+    }else if(keyIsDown(37)){
+        scrollposition += 5;
     }
-    pop();
+    
+    let step = scrollposition / backgroundimages.length;
+    
+
+    for (let i = 0; i < backgroundimages.length; i++) {
+        translate(step, 0);
+        image(backgroundimages[i], -scaledImagewidth, 0, scaledImagewidth, scaledImageheight);
+        image(backgroundimages[i], 0, 0, scaledImagewidth, scaledImageheight);
+        image(backgroundimages[i], scaledImagewidth, 0, scaledImagewidth, scaledImageheight);
+    }
 }
