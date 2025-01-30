a93e5a9bedd4cc695b52c3c48a04e2c977e5eee3
Rainer Stropek <rainer@timecockpit.com>
2025-01-30 09:19:53 +0100

    Saved in TypeScript Playground at 2025-01-30T08:19:53.063Z

---
+++ bubbles_with_functions/index.ts
@@ -0,0 +1,46 @@
+const circles_x: number[] = [];
+const circles_y: number[] = [];
+const circles_diameter: number[] = [];
+
+let waiting_time = 3000;
+let circle_interval: number;
+let level_interval: number;
+
+let points = 0;
+
+function setup() {
+  createCanvas(300, 300);
+  background("black");
+  strokeWeight(2);
+  stroke("lime");
+  noFill();
+
+  addRandomCircle();
+  circle_interval = setInterval(addRandomCircle, waiting_time);
+  
+
+  
+
+  // <<< Add code here
+}
+
+function draw() {
+  background("black");
+
+  // <<< Add code here
+  for(let i = 0; i < circles_x.length; i ++){
+    circle(circles_x[i], circles_y[i], circles_diameter[i]);
+    
+  }
+}
+
+// <<< Add functions here
+function addRandomCircle(){
+  circles_x.push(random(0, width));
+  circles_y.push(random(0, height));
+  circles_diameter.push(random(10, 51));
+}
+
+function isInside(){
+  
+}
