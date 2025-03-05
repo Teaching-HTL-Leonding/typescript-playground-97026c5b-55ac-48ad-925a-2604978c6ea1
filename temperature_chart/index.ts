765ccaed13fe4510c537fe5e2dba9b55fb85e15d
Rainer Stropek <rainer@timecockpit.com>
2025-03-05 10:49:16 +0100

    Saved in TypeScript Playground at 2025-03-05T09:49:15.697Z

--- temperature_chart/index.ts
+++ temperature_chart/index.ts
@@ -6,6 +6,8 @@ const AVG_TEMP_JOHANNESBURG: number[] = [
   20, 20, 18, 15, 12, 9, 9, 12, 16, 18, 18, 19,
 ];
 
+const MONTHS: string[] = ["Jan","Feb","Mar","Apr","May","Jun","Jul"," Aug","Sep","Oct","Nov","Dec"];
+
 function setup() {
   createCanvas(460, 300);
 
@@ -14,18 +16,51 @@ function setup() {
   // 2. Draw axes
   // 3. Draw Y labels
   // 4. Draw X labels
+  drawAxes();
+  drawXLabels();
+  drawYLabels();
+  
 }
 
 function drawAxes() {
   // <<< Add code to draw X and Y axes here
+  strokeWeight(3);
+  line(30, 30, 30, 190);
+
+  line(30, 170, 390, 170);
+
+
 }
 
 function drawYLabels() {
   // <<< Add code to draw labels for the Y axis here
+  for(let i = 0; i < 9; i ++){
+    let ytemp = 35;
+  
+    push();
+    translate(30, 30);
+    translate(0, 20* i);
+    line(- 7, 0, 7, 0);
+    textAlign(CENTER, CENTER);
+    text(ytemp - i * 5, - 20, 0);
+    pop();
+  }
 }
 
 function drawXLabels() {
   // <<< Add code to draw labels for the X axis here
+
+    for(let i = 0; i <= MONTHS.length; i ++){
+      push();
+      translate( 30 + 30 * i, 170);
+      line(0, -7, 0, 7);
+      pop();
+      push()
+      translate(45, 160)
+      textAlign(CENTER, CENTER);
+      text(MONTHS[i], 30 * i, 0);
+      pop(); 
+  }
 }
 
 function drawTemperatures(temperatures: number[]) {
